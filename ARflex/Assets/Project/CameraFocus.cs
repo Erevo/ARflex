5dda53e4c9d5af12a5fd07c0db4e98af5ea00013
Student <student@mirea.ru>
2019-11-22 09:53:02 +0300

    Cloned Working Project

---
+++ ARflex/Assets/Project/CameraFocus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraFocus : MonoBehaviour
+{
+    void Start()
+    {
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+}
